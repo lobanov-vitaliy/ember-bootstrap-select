31be4dd755c6ec772deffcdac046a8271dbfe8f3
Vitaliy Lobanov <lobanov.vitalya@gmail.com>
2017-12-29 01:55:17 +0200

    Updates

--- addon/components/bootstrap-select.js
+++ addon/components/bootstrap-select.js
@@ -2,124 +2,122 @@ import Ember from 'ember';
 import layout from '../templates/components/bootstrap-select';
 
 const {
-    set,
-    observer,
-    on,
-    run,
-    A
+  set,
+  observer,
+  on,
+  run,
+  A
 } = Ember;
 
 export default Ember.Component.extend({
-    layout,
-    tagName: 'select',
-    classNameBindings: ['tick:show-tick'],
-    attributeBindings: [
-        'disabled',
-        'multiple',
-        'title',
-        'width:data-width',
-        'header:data-header',
-        'format:data-selected-text-format',
-        'container:data-container',
-        'search:data-live-search',
-        'actionsBox:data-actions-box'
-    ],
+  layout,
+  tagName: 'select',
+  classNameBindings: ['tick:show-tick'],
+  attributeBindings: [
+    'disabled',
+    'multiple',
+    'title',
+    'width:data-width',
+    'header:data-header',
+    'format:data-selected-text-format',
+    'container:data-container',
+    'search:data-live-search',
+    'actionsBox:data-actions-box'
+  ],
 
-    /**
-     * Append the select to a specific element, e.g. container: 'body'
-     * 
-     * @property container
-     * @type String
-     * @default 'body'
-     * @public
-     */
-    container: 'body',
+  /**
+   * Append the select to a specific element, e.g. container: 'body'
+   * 
+   * @property container
+   * @type String
+   * @default 'body'
+   * @public
+   */
+  container: 'body',
 
-    /**
-     * Disabled select boxes
-     * 
-     * @property disabled
-     * @type Boolean
-     * @default false
-     * @public
-     */
-    disabled: false,
+  /**
+   * Disabled select boxes
+   * 
+   * @property disabled
+   * @type Boolean
+   * @default false
+   * @public
+   */
+  disabled: false,
 
-    /**
-     * Multiple select boxes
-     * 
-     * @property multiple
-     * @type Boolean
-     * @default false
-     * @public
-     */
-    multiple: false,
+  /**
+   * Multiple select boxes
+   * 
+   * @property multiple
+   * @type Boolean
+   * @default false
+   * @public
+   */
+  multiple: false,
 
-    /**
-     * Show the checkmark icon on standard select boxes
-     * 
-     * @property tick
-     * @type Boolean
-     * @default true
-     * @public
-     */
-    tick: true,
+  /**
+   * Show the checkmark icon on standard select boxes
+   * 
+   * @property tick
+   * @type Boolean
+   * @default true
+   * @public
+   */
+  tick: true,
 
-    /**
-     * Collection of all `option-item`s that are children
-     * @property options
-     * @type {Array}
-     */
-    options: A(),
+  /**
+   * Collection of all `option-item`s that are children
+   * @property options
+   * @type {Array}
+   */
+  options: A(),
 
-    change: on(
-        'didInsertElement',
-        observer(
-            'value',
-            function() {
-                this._pickerCall(this._pickerSetValue);
-            }
-        )
-    ),
+  change: on(
+    'didInsertElement',
+    observer(
+      'value',
+      function() {
+        this._pickerCall(this._pickerSetValue);
+      }
+    )
+  ),
 
-    refresh: observer(
-        'disabled',
-        'options.[]',
-        function() {
-            this._pickerCall(this._pickerRefresh);
-        }
-    ),
+  refresh: observer(
+    'disabled',
+    'options.[]',
+    function() {
+      this._pickerCall(this._pickerRefresh);
+    }
+  ),
 
-    getValue() {
-        let value = this.value;
+  getValue() {
+    let value = this.value;
 
-        if (value === undefined ||
-            value === null) {
-            value = '';
-        }
+    if (value === undefined ||
+      value === null) {
+      value = '';
+    }
 
-        return value;
-    },
+    return value;
+  },
 
-    _pickerCall(func) {
-        run.scheduleOnce('afterRender', this, func);
-    },
+  _pickerCall(func) {
+    run.scheduleOnce('afterRender', this, func);
+  },
 
-    _pickerRefresh() {
-        this.$().selectpicker('refresh');
-    },
+  _pickerRefresh() {
+    this.$().selectpicker('refresh');
+  },
 
-    _pickerSetValue() {
-        this.$().selectpicker('val', this.getValue());
-    },
+  _pickerSetValue() {
+    this.$().selectpicker('val', this.getValue());
+  },
 
-    didInsertElement() {
-        let component = this.$();
-        component.selectpicker();
-        component.on('changed.bs.select', () => {
-            set(this, 'value', component.selectpicker('val'));
-        });
-
-        this.isRender = true;
-    },
+  didInsertElement() {
+    let component = this.$();
+    component.selectpicker();
+    component.on('changed.bs.select', () => {
+      set(this, 'value', component.selectpicker('val'));
+    });
+  },
 });
--- tests/integration/components/bootstrap-select-test.js
+++ tests/integration/components/bootstrap-select-test.js
@@ -1,22 +1,22 @@
 import hbs from 'htmlbars-inline-precompile';
 import {
-    test,
-    moduleForComponent
+  test,
+  moduleForComponent
 } from 'ember-qunit';
 import Ember from 'ember';
 
 const {
-    run,
-    set,
-    A
+  run,
+  set,
+  A
 } = Ember;
 
 moduleForComponent('bootstrap-select', {
-    integration: true
+  integration: true
 });
 
 test('Test `Standard` selectpicker', function(assert) {
-    this.render(hbs `
+  this.render(hbs `
         {{#bootstrap-select 
             multiple=false 
             value=value as |select|}}
@@ -29,61 +29,61 @@ test('Test `Standard` selectpicker', function(assert) {
         {{/bootstrap-select}}
     `);
 
-    this.setProperties({
-        value: 1,
-        options: A([{
-            value: 2,
-            label: 'Option 2',
-            disabled: false
-        }, {
-            value: 1,
-            label: 'Option 1',
-            disabled: false
-        }, {
-            value: 3,
-            label: 'Option 3',
-            disabled: false
-        }])
-    });
+  this.setProperties({
+    value: 1,
+    options: A([{
+      value: 2,
+      label: 'Option 2',
+      disabled: false
+    }, {
+      value: 1,
+      label: 'Option 1',
+      disabled: false
+    }, {
+      value: 3,
+      label: 'Option 3',
+      disabled: false
+    }])
+  });
 
-    let component = this.$('select');
-    /* open selectpicker */
-    component.selectpicker('trigger');
+  let component = this.$('select');
+  /* open selectpicker */
+  component.selectpicker('trigger');
 
-    assert.equal(component.selectpicker('val'), 1, 'initialize value.');
-    run(() => {
-        component.selectpicker('val', '3').trigger('change');
-        set(this.options[0], 'disabled', true);
-        set(this.options[0], 'subtext', 'subtext');
-    });
-    assert.equal(this.value, 3, 'change selectpicker value');
-    assert.ok(this.$('option:first').prop('disabled'), 'check disabled element in `select` element');
-    assert.ok(this.$('ul>li:first').hasClass('disabled'), 'check disabled element in `ul` element');
+  assert.equal(component.selectpicker('val'), 1, 'initialize value.');
+  run(() => {
+    component.selectpicker('val', '3').trigger('change');
+    set(this.options[0], 'disabled', true);
+    set(this.options[0], 'subtext', 'subtext');
+  });
+  assert.equal(this.value, 3, 'change selectpicker value');
+  assert.ok(this.$('option:first').prop('disabled'), 'check disabled element in `select` element');
+  assert.ok(this.$('ul>li:first').hasClass('disabled'), 'check disabled element in `ul` element');
 
-    run(() => {
-        set(this.options[0], 'disabled', false);
-    });
-    assert.ok(!this.$('option:first').prop('disabled'));
+  run(() => {
+    set(this.options[0], 'disabled', false);
+  });
+  assert.ok(!this.$('option:first').prop('disabled'));
 
-    run(() => {
-        this.options.removeObject(this.options[this.options.length-1]);
-    });
-    assert.equal(this.$('option').length, 2, 'option has been deleted on `select` element');
-    assert.equal(this.$('ul>li').length, 2, 'option has been deleted on `ul` element');
+  run(() => {
+    this.options.removeObject(this.options[this.options.length - 1]);
+  });
+  assert.equal(this.$('option').length, 2, 'option has been deleted on `select` element');
+  assert.equal(this.$('ul>li').length, 2, 'option has been deleted on `ul` element');
 
-    run(() => {
-        this.options.pushObject({
-            value: 3,
-            label: 'Option 3',
-            disabled: false
-        });
+  run(() => {
+    this.options.pushObject({
+      value: 3,
+      label: 'Option 3',
+      disabled: false
     });
-    assert.equal(this.$('option').length, 3);
-    assert.equal(this.$('ul>li').length, 3);
+  });
+  assert.equal(this.$('option').length, 3);
+  assert.equal(this.$('ul>li').length, 3);
 });
 
 test('Test `Multiple` selectpicker', function(assert) {
-    this.render(hbs `
+  this.render(hbs `
         {{#bootstrap-select 
             multiple=true 
             value=value as |select|}}
@@ -96,30 +96,30 @@ test('Test `Multiple` selectpicker', function(assert) {
         {{/bootstrap-select}}
     `);
 
-    this.setProperties({
-        value: [2,3],
-        options: A([{
-            value: 2,
-            label: 'Option 2',
-            disabled: false
-        }, {
-            value: 1,
-            label: 'Option 1',
-            disabled: false
-        }, {
-            value: 3,
-            label: 'Option 3',
-            disabled: false
-        }])
-    });
+  this.setProperties({
+    value: [2, 3],
+    options: A([{
+      value: 2,
+      label: 'Option 2',
+      disabled: false
+    }, {
+      value: 1,
+      label: 'Option 1',
+      disabled: false
+    }, {
+      value: 3,
+      label: 'Option 3',
+      disabled: false
+    }])
+  });
 
-    let component = this.$('select');
-    /* open selectpicker */
-    component.selectpicker('trigger');
+  let component = this.$('select');
+  /* open selectpicker */
+  component.selectpicker('trigger');
 
-    assert.deepEqual(component.selectpicker('val'), ["2","3"], 'initialize value.');
-    run(() => {
-        component.selectpicker('val', ['1']).trigger('change');
-    });
-    assert.deepEqual(this.value, ['1'], 'change selectpicker value');
+  assert.deepEqual(component.selectpicker('val'), ["2", "3"], 'initialize value.');
+  run(() => {
+    component.selectpicker('val', ['1']).trigger('change');
+  });
+  assert.deepEqual(this.value, ['1'], 'change selectpicker value');
 });
