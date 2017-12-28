0a951aafc93b60f8f68e22f7a0ae15a7b0b21841
Vitaliy Lobanov <vlobanov@acutenet.com>
2017-12-28 20:31:21 +0200

    Initial Commit

--- addon/components/bootstrap-select.js
+++ addon/components/bootstrap-select.js
@@ -1,36 +1,82 @@
 import Ember from 'ember';
 import layout from '../templates/components/bootstrap-select';
 
-const { set, observer, on } = Ember;
+const {
+    set, get,
+    observer,
+    computed,
+    on,
+    run,
+    A
+} = Ember;
 
 export default Ember.Component.extend({
-  layout,
-  tagName: 'select',
-  classNames: ['show-tick'],
+    layout,
+    tagName: 'select',
+    classNameBindings: ['tick:show-tick'],
 
-  attributeBindings: [
-    'multiple',
-    'title',
-    'header:data-header',
-    'format:data-selected-text-format',
-    'container:data-container'
-  ],
+    isRender: false,
 
-  container: 'body',
+    container: 'body',
+    disabled: false,
+    multiple: false,
+    tick:     true,
+    options:  A(),
 
-  handleValueChange: on('didInsertElement', observer('value', function(){
-    this.getComponent().selectpicker('val', this.value);
-  })),
+    attributeBindings: [
+        'disabled',
+        'multiple',
+        'title',
+        'header:data-header',
+        'format:data-selected-text-format',
+        'container:data-container',
+        'search:data-live-search'
+    ],
 
-  didInsertElement(){
-    let component = this.getComponent();
-    component.selectpicker();
-    component.on('changed.bs.select', () => {
-      set(this, 'value', component.val());
-    });
-  },
+    change: on(
+        'didInsertElement', 
+        observer(
+            'value', 
+            function() {
+                this.getComponent().selectpicker('val', this.getValue());
+            }
+        )
+    ),
 
-  getComponent(){
-    return this.$();
-  }
+    refresh: observer(
+        'disabled',
+        'options.[]',
+        function(){
+            run.scheduleOnce('afterRender', this, this._selectpickerRefresh);
+        }
+    ),
+
+    didInsertElement() {
+        let component = this.getComponent();
+        component.selectpicker();
+        component.on('changed.bs.select', () => {
+            set(this, 'value', component.selectpicker('val'));
+        });
+
+        this.isRender = true;
+    },
+
+    getValue() {
+        let value = this.value;
+
+        if (value === undefined || 
+            value === null) {
+            value = '';
+        }
+
+        return value;
+    },
+
+    getComponent() {
+        return this.$();
+    },
+
+    _selectpickerRefresh(){
+        this.getComponent().selectpicker('refresh');
+    }
 });
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,5 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	value:2
+    value: 1,
+    options: Ember.A([{
+        value: 1,
+        label: 'Label: 1',
+        disabled:false
+    },{
+        value: 2,
+        label: 'Label: 2',
+        disabled:false
+    },{
+        value: 3,
+        label: 'Label: 3',
+        disabled:false
+    },{
+        value: 4,
+        label: 'Label: 4',
+        disabled:false
+    },{
+        value: 5,
+        label: 'Label: 5',
+        disabled:false
+    },{
+        value: 6,
+        label: 'Label: 6',
+        disabled:false
+    },{
+        value: 7,
+        label: 'Label: 7',
+        disabled:false
+    },{
+        value: 8,
+        label: 'Label: 8',
+        disabled:false
+    }])
 });
